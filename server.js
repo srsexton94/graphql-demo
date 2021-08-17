1a56aee7873f8f10c4ad9a961531acc12a7b9dbc
Samantha Sexton <50561643+srsexton94@users.noreply.github.com>
2021-08-17 13:59:24 -0400

    Create root query for pokemon

--- server.js
+++ server.js
@@ -1,20 +1,34 @@
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
-const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require('graphql')
+const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLList, GraphQLInt, GraphQLNonNull } = require('graphql')
+
+const { pokemon } = require('./data/pokemon.json')
+// const { trainers } = require('./data/trainers.json')
 
 const app = express()
-const name = 'HelloWorld'
 
-const query = new GraphQLObjectType({
-  name,
+const PokemonType = new GraphQLObjectType({
+  name: 'Pokemon',
+  description: 'A single pokemon trained by a trainer',
   fields: () => ({ 
-    message: { 
-      type: GraphQLString, 
-      resolve: () => name 
+    id: { type: GraphQLNonNull(GraphQLInt) },
+    name: { type: GraphQLNonNull(GraphQLString)},
+    trainerId: { type: GraphQLNonNull(GraphQLInt) }
+  })
+})
+
+const RootQueryType = new GraphQLObjectType({
+  name: 'Query',
+  description: 'Root query',
+  fields: () => ({
+    pokemon: {
+      type: new GraphQLList(PokemonType),
+      description: 'List of All Pokemon',
+      resolve: () => pokemon
     }
   })
 })
-const schema = new GraphQLSchema({ query })
+const schema = new GraphQLSchema({ query: RootQueryType })
 
 app.use('/graphql', graphqlHTTP({ schema, graphiql: true }))
 app.listen(5000, () => console.log('Server running'))
